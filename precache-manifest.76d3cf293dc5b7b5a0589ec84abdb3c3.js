cedf601fafc667fe23a581edc8345712887ebfe0
João Pedro Barberino Silva <joao.p.barberino@gmail.com>
2019-05-31 10:43:09 -0300

    Updates

---
+++ precache-manifest.76d3cf293dc5b7b5a0589ec84abdb3c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860bc86cab7975f4c77c79d0cf6ccdf3",
+    "url": "/ReactYoutubeApi/index.html"
+  },
+  {
+    "revision": "767842bf65b7f2380e29",
+    "url": "/ReactYoutubeApi/static/css/main.96c11df6.chunk.css"
+  },
+  {
+    "revision": "c8d6094a958a651e58fa",
+    "url": "/ReactYoutubeApi/static/js/2.3d6dc8b9.chunk.js"
+  },
+  {
+    "revision": "767842bf65b7f2380e29",
+    "url": "/ReactYoutubeApi/static/js/main.142e34bf.chunk.js"
+  },
+  {
+    "revision": "737f86ed8e15964a5b1e",
+    "url": "/ReactYoutubeApi/static/js/runtime~main.3a8b5402.js"
+  }
+]);
